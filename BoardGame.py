ceb894d9df892b3ed453b9bf4ef78d7e39a68021
xuan1885322434@gmail.com <xuan1885322434@gmail.com>
2025-02-26 22:23:22 +0700

    boardgame_level1-5

--- BoardGame.py
+++ BoardGame.py
@@ -1,54 +1,41 @@
 from board import boards
 import pygame
 import math
+import os
 
 #khởi tạo Pygame
 pygame.init()
 
 #tạo kích thước màn hình
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 890, 850
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pacman with Tom and Jerry")
 font = pygame.font.Font(None, 20)
 
+GRID_SIZE = 30  # Kích thước mỗi ô vuông
 #tốc độ FPS
 timer = pygame.time.Clock()
 FPS = 60
 PI = math.pi
+
 #chọn level 1
 color = 'pink'
-level = boards
 def draw_board():
-    num1 = ((HEIGHT - 50) // len(boards))
-    num2 = (WIDTH // len(boards[0]))
-    for i in range(len(level)):
-        for j in range(len(level[i])):
-            if level [i][j] == 1:
-                pygame.draw.circle(screen, 'lightgreen', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
-            if level [i][j] == 2:
-                pygame.draw.circle(screen, 'yellow', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
-            if level [i][j] == 3:
-                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
-                                (j * num2 + (0.5 * num2), i * num1 + num1), 3)
-            if level[i][j] == 4:
-                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
-                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
-            if level[i][j] == 5:
-                pygame.draw.arc(screen, color, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
-                                0, PI / 2, 3)
-            if level[i][j] == 6:
-                pygame.draw.arc(screen, color,
-                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
-            if level[i][j] == 7:
-                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
-                                3 * PI / 2, 3)
-            if level[i][j] == 8:
-                pygame.draw.arc(screen, color,
-                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
-                                2 * PI, 3)
-            if level[i][j] == 9:
-                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
-                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+    maze_width = len(boards[0]) * GRID_SIZE
+    maze_height = len(boards) * GRID_SIZE
+    offset_x = (WIDTH - maze_width) // 2
+    offset_y = (HEIGHT - maze_height) // 2
+
+    for row in range(len(boards)):
+        for col in range(len(boards[row])):
+            x, y = offset_x + col * GRID_SIZE, offset_y + row * GRID_SIZE
+            if boards[row][col] == 1:
+                pygame.draw.rect(screen, 'pink', (x, y, GRID_SIZE, GRID_SIZE))
+            elif boards[row][col] == 2:
+                pygame.draw.circle(screen, 'white', (x + GRID_SIZE // 2, y + GRID_SIZE // 2), 5)
+            elif boards[row][col] == 3:
+                pygame.draw.circle(screen, 'yellow', (x + GRID_SIZE // 2, y + GRID_SIZE // 2), 10)
 
 running = True
 while running:
